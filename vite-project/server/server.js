1518ac3be19004870d6b507d34af11dd037cfd32
Marco Filannino <filanninobusiness@gmail.com>
2025-02-16 15:58:05 +0100

    Aggiunto Get che richiama la funzione per msotrare i pianeti.

--- vite-project/server/server.js
+++ vite-project/server/server.js
@@ -1,9 +1,19 @@
 import express, { json } from "express";
+import db from "./db.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(json());
+
+app.get("/", (req, res) => {
+  db.all(`SELECT * FROM planets`, (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json(rows);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`server in ascolto su http://localhost:${PORT}`);
